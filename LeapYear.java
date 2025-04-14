e95d003a09a74b437990499a46a05b1cc2f3d114
Balasaheb Shelake <117717370+Balasaheb2218@users.noreply.github.com>
2025-04-14 13:15:21 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -3,7 +3,7 @@
 public class LeapYear {
 	public static void main(String[] args) {
 		int n = 2025;
-				if((n%4==0&&n%100!=0)||n%400==0) {
+				if((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
 			System.out.println(n+" is leap year");
 		}
 		else {
